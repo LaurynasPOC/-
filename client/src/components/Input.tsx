b9849c8c3880b05ba46d0587906cffcb93867041
Laurynas <laurynas.pociunas@gmail.com>
2024-02-04 11:57:35 +0200

    registration form

--- client/src/components/Input.tsx
+++ client/src/components/Input.tsx
@@ -2,12 +2,13 @@ import React, { ChangeEvent } from "react";
 import styled from "styled-components";
 
 interface InputProps {
-  label?: string;
+  label: string;
   value: string;
   onChange: (event: ChangeEvent<HTMLInputElement>) => void;
   type: "email" | "text" | "password" | "number";
   pattern?: string;
   required?: boolean;
+  errorMessage?: string;
 }
 
 const Input: React.FC<InputProps> = ({
@@ -17,9 +18,10 @@ const Input: React.FC<InputProps> = ({
   pattern,
   type,
   required,
+  errorMessage,
   ...rest
 }) => (
-  <StyledInput {...rest}>
+  <StyledInput errorMessage={errorMessage} {...rest}>
     <input
       id={label?.replace(" ", "").toLowerCase()}
       name={label}
@@ -30,37 +32,41 @@ const Input: React.FC<InputProps> = ({
       required={required}
       {...rest}
     />
-    {label && (
-      <label htmlFor={label.replace(" ", "").toLowerCase()}>{label}</label>
-    )}
+    <label htmlFor={label?.replace(" ", "").toLowerCase()}>{label}</label>
+    {errorMessage && <p>{errorMessage}</p>}
   </StyledInput>
 );
 
 export default Input;
 
-const StyledInput = styled.div`
+interface StyleProps {
+  errorMessage?: string;
+}
+
+const StyledInput = styled.div<StyleProps>`
   position: relative;
   margin: 0 auto;
-  margin-bottom: 10px;
+  margin-bottom: 25px;
   width: 100%;
   max-width: 400px;
 
   label {
     position: absolute;
     left: 0;
-    padding: 10px 15px;
+    padding: 15px;
     color: var(--secondary);
     pointer-events: none;
     transition: 0.4s;
   }
   input {
-    padding: 10px 15px;
+    padding: 15px;
     background: var(--white);
     color: var(--black);
-    border: 1px solid var(--primary);
+    border: ${({ errorMessage }) =>
+      errorMessage ? "1px solid var(--error)" : "1px solid var(--primary)"};
     border-radius: 4px;
     width: 100%;
-    font-size: 14px;
+    font-size: 16px;
     transition: 0.6s;
     &:focus,
     &:hover {
@@ -68,8 +74,7 @@ const StyledInput = styled.div`
       outline: none;
     }
   }
-  input:focus + label,
-  input:valid + label {
+  input:focus + label {
     color: var(--grey);
     transform: translateY(-10px) translateX(10px);
     font-size: 12px;
@@ -77,5 +82,14 @@ const StyledInput = styled.div`
     padding: 2px;
     font-weight: 500;
     letter-spacing: 0.05rem;
+  }
+  p {
+    position: absolute;
+    font-weight: 500;
+    letter-spacing: 0.05rem;
+    font-size: 12px;
+    left: 10px;
+    bottom: -30px;
+    color: var(--error);
   }
 `;
--- client/src/pages/register/Register.tsx
+++ client/src/pages/register/Register.tsx
@@ -1,5 +1,124 @@
-const Register = () => {
-  return <div>Register</div>;
-};
+import { useState } from "react";
+import styled from "styled-components";
+import Input from "../../components/Input";
+import Button from "../../components/Buttons";
+import { SectionWrapper, Container } from "../../components/wrappers";
 
+const RegisterFormWrapper = styled(SectionWrapper)`
+  padding: 20px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  h4 {
+    color: var(--primary);
+    margin-bottom: 20px;
+  }
+`;
+
+interface FormData {
+  username: string;
+  email: string;
+  password: string;
+  repeatPassword: string;
+}
+
+interface FormErrors {
+  username?: string;
+  email?: string;
+  password?: string;
+  repeatPassword?: string;
+}
+const Register: React.FC = () => {
+  const [formData, setFormData] = useState<FormData>({
+    username: "",
+    email: "",
+    password: "",
+    repeatPassword: "",
+  });
+  const [errors, setErrors] = useState<FormErrors>({});
+
+  const handleInputChange =
+    (field: keyof FormData) => (e: React.ChangeEvent<HTMLInputElement>) => {
+      setFormData({ ...formData, [field]: e.target.value });
+
+      setErrors({ ...errors, [field]: "" });
+    };
+  const validateForm = (): boolean => {
+    const newErrors: FormErrors = {};
+    let formIsValid: boolean = true;
+
+    if (!formData.username) {
+      newErrors.username = "Vartotojo vardas yra privalomas.";
+      formIsValid = false;
+    }
+
+    if (!formData.email) {
+      newErrors.email = "El. pašto adresas yra privalomas.";
+      formIsValid = false;
+    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+      newErrors.email = "Neteisingas el. pašto formatas.";
+      formIsValid = false;
+    }
+
+    if (!formData.password) {
+      newErrors.password = "Slaptažodis yra privalomas.";
+      formIsValid = false;
+    }
+
+    if (formData.password !== formData.repeatPassword) {
+      newErrors.repeatPassword = "Slaptažodžiai nesutampa.";
+      formIsValid = false;
+    }
+
+    setErrors(newErrors);
+    return formIsValid;
+  };
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (validateForm()) {
+      console.log("Form data:", formData);
+    }
+  };
+  return (
+    <SectionWrapper>
+      <Container>
+        <RegisterFormWrapper as="form" onSubmit={handleSubmit}>
+          <h4>Registracijos duomenys</h4>
+          <Input
+            value={formData.username}
+            onChange={handleInputChange("username")}
+            type="text"
+            label="Vartotojo vardas"
+            errorMessage={errors.username}
+          />
+          <Input
+            value={formData.email}
+            onChange={handleInputChange("email")}
+            type="email"
+            label="El. paštas"
+            errorMessage={errors.email}
+          />
+          <Input
+            value={formData.password}
+            onChange={handleInputChange("password")}
+            label="Slaptažodis"
+            type="password"
+            errorMessage={errors.password}
+          />
+          <Input
+            value={formData.repeatPassword}
+            onChange={handleInputChange("repeatPassword")}
+            label="Pakartokite slaptažodį"
+            type="password"
+            errorMessage={errors.repeatPassword}
+          />
+          <Button variant="primary" type="submit">
+            Registruotis
+          </Button>
+        </RegisterFormWrapper>
+      </Container>
+    </SectionWrapper>
+  );
+};
 export default Register;
