7fe0ed8e62e98648add741c1ff7c5467e545fc24
Laurynas <laurynas.pociunas@gmail.com>
2024-02-04 21:26:27 +0200

    mysql data

--- server/app.js
+++ server/app.js
@@ -1,13 +1,15 @@
+require("dotenv").config(); // This line should be at the top
+
 const express = require("express");
 const mysql = require("mysql");
 
 const app = express();
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "mysql",
-  database: "",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
 });
 
 db.connect((err) => {
